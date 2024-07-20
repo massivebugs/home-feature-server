742857fb3f8fe71a4d9be7cca74951e6327754e3
massivebugs <massivebugs@protonmail.com>
2024-07-21 00:40:14 +0900

    write some tests

---
+++ internal/api/validate_test.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"testing"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+type ValidatableStruct struct {
+	SomeRequiredField string
+}
+
+func (s *ValidatableStruct) Validate() error {
+	return validation.ValidateStruct(
+		s,
+		validation.Field(
+			&s.SomeRequiredField,
+			validation.Required,
+		),
+	)
+}
+
+type StructWithoutValidation struct{}
+
+func TestRequestValidator_Validate(t *testing.T) {
+	type args struct {
+		i interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "validation success",
+			args: args{
+				i: &ValidatableStruct{
+					SomeRequiredField: "foo",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "validation error",
+			args: args{
+				i: &ValidatableStruct{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "error because struct does not implement validate.Validatable",
+			args: args{
+				i: &StructWithoutValidation{},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &RequestValidator{}
+			if err := r.Validate(tt.args.i); (err != nil) != tt.wantErr {
+				t.Errorf("RequestValidator.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
